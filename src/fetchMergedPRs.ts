ea616afeaeda3fc4c048e30a1b2d69d52248174a
uto-usui <me@uto-usui.com>
2023-09-17 17:48:16 +0900

    Refactor fetchMergedPRs for better readability and stability
    The fetchMergedPRs.ts file has been refactored to improve code readability and error handling. The changes include splitting up large blocks of code into smaller, reusable functions for easier understanding and debugging. Some repeated code related to date calculation have been moved into a helper function, thus reducing redundancy. Also, async/await is now used for handling asynchronous operations instead of .then() and .catch(), providing better error handling and making the code look more synchronous and easier to follow. If an error occurs now it's always logged, not just in some parts of the application.

--- src/fetchMergedPRs.ts
+++ src/fetchMergedPRs.ts
@@ -1,84 +1,65 @@
 import 'dotenv/config'
 import fs from 'fs'
 import path from 'path'
-
 import { WebClient } from '@slack/web-api'
 
-// Get slack token from environment variables
-const slackToken = process.env.SLACK_TOKEN
-const channel = process.env.SLACK_CHANNEL ?? ''
-
-// Initialize slack web client
-const web = new WebClient(slackToken)
-
-// Token for authentication. Replace with your token.
+const web = new WebClient(process.env.SLACK_TOKEN)
+const dir = 'out'
 const token = process.env.GITHUB_TOKEN
 const userName = process.env.GITHUB_USER_NAME
-
-// Get current date as millisecond
-const nowMs = Date.now()
-
-// JST is ahead of UTC by 9 hours
+const channel = process.env.SLACK_CHANNEL ?? ''
 const offsetMs = 9 * 60 * 60 * 1000
 
-// Get today's date in JST
-const today = new Date(nowMs + offsetMs)
+const getDateString = (days: number) => {
+  const date = new Date(Date.now() + offsetMs - days * 24 * 60 * 60 * 1000)
+  return date.toISOString().split('T')[0]
+}
 
-// Get the date one week ago in JST
-const since = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000)
+const sendMessageToSlack = async (message: string) => {
+  const res = await web.chat.postMessage({ channel, text: message })
+  console.log('メッセージが送信されました: ', res.ts)
+}
 
-// Get YYYY-MM-DD format
-const formatDate = (date: Date): string => date.toISOString().split('T')[0]
-const sinceFormatted = formatDate(since)
-const todayFormatted = formatDate(today)
+const fetchPRsAndWriteToFile = async () => {
+  const sinceFormatted = getDateString(7)
+  const todayFormatted = getDateString(0)
 
-fetch(
-  `https://api.github.com/search/issues?q=author:${userName}+merged:${sinceFormatted}..${todayFormatted}`,
-  {
-    method: 'GET',
-    headers: {
-      Authorization: `token ${token}`,
-      Accept: 'application/vnd.github.v3+json'
+  const response = await fetch(
+    `https://api.github.com/search/issues?q=author:${userName}+merged:${sinceFormatted}..${todayFormatted}`,
+    {
+      method: 'GET',
+      headers: { Authorization: `token ${token}`, Accept: 'application/vnd.github.v3+json' }
     }
+  )
+
+  const data = await response.json()
+
+  const items = data.items.map((item: any) => ({
+    title: item.title,
+    closed_at: item.closed_at,
+    url: item.url,
+    body: item.body
+  }))
+
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
   }
-)
-  .then((response) => response.json())
-  .then((data) => {
-    const items = data.items.map((item: any) => ({
-      title: item.title,
-      closed_at: item.closed_at,
-      url: item.url,
-      body: item.body
-    }))
 
-    const dir = 'out'
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir)
-    }
+  const filePath = path.join(dir, `mergedPRs_${sinceFormatted}_to_${todayFormatted}.json`)
+  fs.writeFileSync(filePath, JSON.stringify(items, null, 2), 'utf8')
 
-    const filePath = path.join(dir, `mergedPRs_${sinceFormatted}_to_${todayFormatted}.json`)
+  console.log('ファイルが作成されました')
 
-    fs.writeFile(filePath, JSON.stringify(items, null, 2), 'utf8', (error) => {
-      if (error) {
-        console.error(error)
-        return
-      }
-      console.log('File has been created')
-    })
+  const formattedMessage = items
+    .map(
+      (item: any, index: number) =>
+        `*#${index + 1} 🔖 ${item.title}*\n ✔️ ${item.closed_at}\n 🔗 ${item.url}\n 📝 ${
+          item.body ?? '🐈'
+        }\n\n`
+    )
+    .join('\n')
 
-    const formattedMessage = items
-      .map((item: any, index: number) => {
-        return `*#${index + 1} PR Title: ${item.title}*\nClosed At: ${item.closed_at}\nURL: ${
-          item.url
-        }\nBody: ${item.body}\n\n`
-      })
-      .join('\n')
+  sendMessageToSlack(formattedMessage).catch((error) => console.error(error))
+}
 
-    ;(async () => {
-      // See: https://api.slack.com/methods/chat.postMessage
-      const res = await web.chat.postMessage({ channel, text: formattedMessage })
-
-      console.log('Message sent: ', res.ts)
-    })()
-  })
-  .catch((error) => console.error(error))
+fetchPRsAndWriteToFile().catch((error) => console.error('エラーが発生しました：', error))
